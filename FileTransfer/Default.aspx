1844c49102ab2793b1eb91b0c1745aaadd974b39
Freditansari <freditansari@gmail.com>
2015-11-24 23:07:08 +0700

    First commit

---
+++ FileTransfer/Default.aspx
@@ -0,0 +1,122 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FileTransfer.Default" %>
+
+<%@ Register assembly="Telerik.Web.UI" namespace="Telerik.Web.UI" tagprefix="telerik" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 214px;
+        }
+        .auto-style2 {
+            width: 299px;
+        }
+        .auto-style3 {
+            width: 214px;
+            height: 158px;
+        }
+        .auto-style4 {
+            width: 299px;
+            height: 158px;
+        }
+        .auto-style5 {
+            height: 158px;
+        }
+        .auto-style6 {
+            width: 214px;
+            height: 131px;
+        }
+        .auto-style7 {
+            width: 299px;
+            height: 131px;
+        }
+        .auto-style8 {
+            height: 131px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <telerik:RadScriptManager ID="ScriptManager1" runat="server" EnableTheming="True">
+            <Scripts>
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js">
+                </asp:ScriptReference>
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js">
+                </asp:ScriptReference>
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js">
+                </asp:ScriptReference>
+            </Scripts>
+        </telerik:RadScriptManager>
+    <div>
+    
+        <table style="width:100%;">
+            <tr>
+                <td class="auto-style1">From:</td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="TextBox1" runat="server" Width="289px"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">To:</td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="TextBox2" runat="server" Width="287px"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Message:</td>
+                <td class="auto-style4">
+                    <asp:TextBox ID="TextBox3" runat="server" Height="130px" Width="292px"></asp:TextBox>
+                </td>
+                <td class="auto-style5"></td>
+            </tr>
+            <tr>
+                <td class="auto-style6">Files:</td>
+                <td class="auto-style7">
+    
+        <telerik:RadAsyncUpload ID="FileUploader" runat="server" Height="33px" Width="275px" MaxFileSize="2000000000" MultipleFileSelection="Automatic" OnFileUploaded="FileUploader_FileUploaded" TargetFolder="Uploaded" TemporaryFileExpiration="02:00:00" TemporaryFolder="Temp">
+        </telerik:RadAsyncUpload>
+                    <br />
+    
+                </td>
+                <td class="auto-style8"></td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td class="auto-style2">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td class="auto-style2">
+                    <asp:Button ID="SendButton" runat="server" OnClick="SendButton_Click" Text="Send!" />
+                    <br />
+                </td>
+                <td>
+                    <asp:Panel ID="ValidFiles" Visible="false" runat="server" CssClass="qsf-success">
+                    <h3>You successfully uploaded:</h3>
+                    <ul class="qsf-list" runat="server" id="ValidFilesList"></ul>
+                </asp:Panel>
+ 
+                <asp:Panel ID="InvalidFiles" Visible="false" runat="server" CssClass="qsf-error">
+                    <h3>The Upload failed for:</h3>
+                    <ul class="qsf-list ruError" runat="server" id="InValidFilesList">
+                        <li>
+                            <p class="ruErrorMessage">The size of your overall upload exceeded the maximum of <asp:Label ID="MaxFileSizeLabel" runat="server" ></asp:Label> MB</p>
+                        </li>
+                    </ul>
+                    </asp:Panel>
+                    <asp:GridView ID="GridView1" runat="server">
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ FileTransfer/Default.aspx.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+
+namespace FileTransfer
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string uri = HttpContext.Current.Request.Url.Host;
+
+        int ExpiryDays = 7;
+        long totalBytes;
+
+        double maxTotalUploadSize = 2000000000;
+        //double maxTotalUploadSize = 1000000;
+
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MaxFileSizeLabel.Text = maxTotalUploadSize.ToString();
+        }
+
+        private List<CustomUploadedFileInfo> uploadedFiles = new List<CustomUploadedFileInfo>();
+
+        public List<CustomUploadedFileInfo> UploadedFiles
+        {
+            get
+            {
+                return uploadedFiles;
+            }
+            set
+            {
+                uploadedFiles = value;
+            }
+        }
+
+        public class CustomUploadedFileInfo
+        {
+            public string FileName { get; set; }
+
+            public string FileExtension { get; set; }
+
+            public Int64 ContentLength { get; set; }
+
+            public string LinkName { get; set; }
+
+            public string batchUploadNumber { get; set; }
+
+            public DateTime DateUploaded { get; set; }
+
+            public DateTime dateExpired { get; set; }
+
+        }
+
+        //private void PopulateUploadedFilesList()
+        //{
+        //    string batchUpload = Guid.NewGuid().ToString();
+        //    DateTime today = DateTime.Now;
+            
+
+        //    foreach (UploadedFile file in FileUploader.UploadedFiles)
+        //    {
+        //        CustomUploadedFileInfo uploadedFileInfo = new CustomUploadedFileInfo();
+
+        //        uploadedFileInfo.FileName = file.GetName();
+        //        uploadedFileInfo.FileExtension = file.GetExtension().Replace(".", string.Empty);
+        //        uploadedFileInfo.ContentLength = file.ContentLength / 1024;
+        //        uploadedFileInfo.LinkName = uri + "/Uploaded/" + uploadedFileInfo.FileName;
+        //        uploadedFileInfo.batchUploadNumber = batchUpload;
+        //        uploadedFileInfo.DateUploaded = today;
+        //        uploadedFileInfo.dateExpired = today.AddDays(ExpiryDays);
+
+        //        UploadedFiles.Add(uploadedFileInfo);
+        //    }
+        //}
+        protected void FileUploader_FileUploaded(object sender, Telerik.Web.UI.FileUploadedEventArgs e)
+        {
+            //todo check max limit?
+
+            //double totalFilesSize = uploadedFiles.Sum(item => item.ContentLength);
+
+            //if (totalFilesSize >= maxTotalUploadSize)
+            //{
+            //    SendButton.Enabled = false;
+            //}
+
+            //const int MaxTotalBytes = 52428800; // 50 MB
+            string batchUpload = Guid.NewGuid().ToString();
+            DateTime today = DateTime.Now;
+
+            var liItem = new HtmlGenericControl("li");
+            liItem.InnerText = e.File.FileName;
+            //totalBytes = e.File.ContentLength;
+            totalBytes += e.File.ContentLength;
+
+            if (totalBytes < maxTotalUploadSize)
+            {
+                // Total bytes limit has not been reached, accept the file
+                e.IsValid = true;
+                totalBytes += e.File.ContentLength;
+               
+            }
+            else
+            {
+                // Limit reached, discard the file
+                e.IsValid = false;
+            }
+
+            if (e.IsValid)
+            {
+
+                ValidFiles.Visible = true;
+                ValidFilesList.Controls.AddAt(0, liItem);
+                //PopulateUploadedFilesList();
+
+                 //foreach (UploadedFile file in FileUploader.UploadedFiles)
+        //    {
+                CustomUploadedFileInfo uploadedFileInfo = new CustomUploadedFileInfo();
+
+                uploadedFileInfo.FileName = e.File.FileName;
+                //uploadedFileInfo.FileExtension = file.GetExtension().Replace(".", string.Empty);
+                uploadedFileInfo.FileExtension = e.File.GetExtension().Replace(".", string.Empty);
+                uploadedFileInfo.ContentLength = e.File.ContentLength / 1024;
+                uploadedFileInfo.LinkName = uri + "/Uploaded/" + uploadedFileInfo.FileName;
+                uploadedFileInfo.batchUploadNumber = batchUpload;
+                uploadedFileInfo.DateUploaded = today;
+                uploadedFileInfo.dateExpired = today.AddDays(ExpiryDays);
+
+                UploadedFiles.Add(uploadedFileInfo);
+        //    }
+
+            }
+            else
+            {
+
+                InvalidFiles.Visible = true;
+                InValidFilesList.Controls.AddAt(0, liItem);
+            }
+            
+
+
+
+        }
+
+        protected void SendButton_Click(object sender, EventArgs e)
+        {
+           
+
+            //PopulateUploadedFilesList();
+            Page.Validate();
+            
+            if (UploadedFiles != null && Page.IsValid)
+            {
+                //todo email action with files and link maker
+                //todo there is something to do but I forgot.
+                GridView1.DataSource = uploadedFiles;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
